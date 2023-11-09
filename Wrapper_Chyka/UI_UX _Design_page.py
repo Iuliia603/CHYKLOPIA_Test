079622d48843ffe7005658e5550eae848371a111
Iuliia603 <iuliia60358@gmail.com>
2023-11-09 15:45:41 -0500

    Create UI_UX _Design_page.py

---
+++ Wrapper_Chyka/UI_UX _Design_page.py
@@ -0,0 +1,19 @@
+from CHYKALOPIA.Wrapper_Chyka.Header import Header
+from CHYKALOPIA.Wrapper_Chyka.Home_Page import HomePage
+from CHYKALOPIA.Wrapper_Chyka.UIElement import UIElement as Element
+from selenium.webdriver.common.by import By
+
+
+class UIUXPage:
+    def __init__(self, browser):
+        self.header = Header(browser)
+        self.homepage = HomePage(browser)
+
+        self.ui_ux_title = Element(browser, By.XPATH,'//*[@id="content"]/div/div[1]/section[1]/div[2]/div[1]/div/div[2]/div/h2')
+
+    def get_ui_ux_title(self):
+        return self.get_ui_ux_title().get_text()
+
+
+
+
